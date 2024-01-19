12f341ed5122157d53971bd41754ead0fa9bd1d2
Theglxry <102861719+Theglxry@users.noreply.github.com>
2024-01-19 01:09:19 +0100

    update search.jsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,28 +1,45 @@
-'use client  '
+import { SearchManuFacturerProps } from "@/types";
+import { Combobox, Transition } from "@headlessui/react";
+import { useState, Fragment } from "react";
+import Image from "next/image";
 
-import { SearchManuFacturerProps } from '@/types'
-import { Combobox, Transition} from '@headlessui/react'
-import Image from 'next/image'
+const SearchManufacturer = ({
+  manufacturer,
+  setManuFacturer,
+}: SearchManuFacturerProps) => {
+  const [searchQuery, setSearchQuery] = useState('');
 
-
-
-
-const SearchManufacturer = ({manufacturer, setManuFacturer} : SearchManuFacturerProps ) => {
   return (
-    <div className='search-manufacturer'> 
-    <Combobox>
-      <div className='relative w-full'>
-      <Combobox.Button className="absolut top-[14px]">
-        <Image src="/car-logo.svg" alt="car logo" 
-        width={28} 
-        height={28} 
-        className='ml-4'
-         />
-      </Combobox.Button>
-      </div>
-    </Combobox>
-    </div>
-  )
-}
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px] sm:pr-4">
+            <Image
+              src="/car-logo.svg"
+              alt="car logo"
+              width={28}
+              height={28}
+              className="ml-4"
+            />
+          </Combobox.Button>
 
-export default SearchManufacturer
+          <Combobox.Input
+            className="search-manufacturer__input" // Adjust padding as needed
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+
+          <Transition 
+          as={Fragment}
+
+          >
+           
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacturer;
